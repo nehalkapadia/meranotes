87d38caf69263fb6a550e1061f9ba7cb4c216370
nehalkapadiya <nehal.kapadia@mobiosolutions.com>
2022-04-27 13:57:57 +0530

    changed file location

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:js';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meranotes/firebase_options.dart';
-import 'package:meranotes/verify_email.dart';
+import 'package:meranotes/views/verify_email.dart';
 import 'package:meranotes/views/login_view.dart';
 import 'package:meranotes/views/register_view.dart';
 import 'dart:developer' as devtools show log;
